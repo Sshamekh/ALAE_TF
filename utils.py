99b6685fe4fc948c9beb505282f38bcea931d817
Sara Shamekh <56313667+Sshamekh@users.noreply.github.com>
2022-04-13 11:23:18 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,32 +1,23 @@
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import tensorflow as tf
+import numpy as np
+def bin_threshold(data1,data2,nbins):
+    minpw = data1.min()
+    maxpw = data1.max()
+    binsize = ((maxpw - minpw)/nbins)
+    print (minpw,maxpw,binsize)
+    bined_pw = []
+    bined_precip = []
+    std1 = []
+    std2 = []
+    for i in range(nbins+1):
+        th1 = minpw+i*binsize 
+        th2 = minpw+(i+1)*binsize 
+        indecies = np.where((data1>=th1) & (data1<th2))[0]
+        if len(indecies)>100:
+            bined_pw.append(np.mean(data1[indecies]))
+            bined_precip.append(np.mean(data2[indecies]))
+            std1.append(np.std(data1[indecies]))
+            std2.append(np.std(data2[indecies]))
+            
+    return bined_pw,bined_precip,std1,std2
 
-def tf_process_images(image):
 
-  img_shaped =  tf.dtypes.cast(tf.reshape(image, [-1, 28, 28, 1]), tf.float32)
-  img_shaped_resized = tf.image.resize(img_shaped, [32, 32])
-  img_shaped_resized = (img_shaped_resized / 127.5) - 1
-
-  return img_shaped_resized
-
-def plot_mnist_grid(samples, target_file = None):
-
-    # 8 x 8 grid
-    fig = plt.figure(figsize=(8, 8))
-    gs = gridspec.GridSpec(8, 8)
-    gs.update(wspace=0.05, hspace=0.05)
-
-    for i, sample in enumerate(samples):
-        ax = plt.subplot(gs[i])
-        plt.axis('off')
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.set_aspect('equal')
-        plt.imshow(sample.reshape(32, 32), cmap='gray')
-
-    if target_file is not None:
-        plt.savefig(target_file, bbox_inches="tight")
-        plt.close(fig)
-    else:
-        return fig
